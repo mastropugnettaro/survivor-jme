fb66cf9425f98a5d980b31fc2091eb5303fa6eff
epixloot@gmail.com <epixloot@gmail.com@5f3b1f02-cc30-94c8-37e5-56b26f70758c>
2012-01-11 00:04:42 +0000

    one pass test

---
+++ ProjectiveTextureMapping/src/com/jme3/post/TextureProjectorRenderer1pass.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2009-2010 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.post;
+
+import com.jme3.material.Material;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.asset.AssetManager;
+import com.jme3.material.MatParam;
+import com.jme3.material.RenderState;
+import com.jme3.math.Matrix4f;
+import com.jme3.math.Vector3f;
+import com.jme3.post.SceneProcessor;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.renderer.lwjgl.LwjglRenderer;
+import com.jme3.renderer.queue.GeometryList;
+import com.jme3.shader.Shader;
+import com.jme3.shader.Uniform;
+import com.jme3.shader.VarType;
+import com.jme3.texture.FrameBuffer;
+import com.jme3.texture.Texture2D;
+import com.jme3.texture.TextureArray;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A SceneProcessor that renders TextureProjectors, which means it projects 
+ * textures on scene geometry.
+ * 
+ * @author survivor
+ */
+public class TextureProjectorRenderer1pass implements SceneProcessor 
+{
+  private RenderManager renderManager;
+  private ViewPort viewPort;
+  private Material textureMat;
+  private ArrayList<TextureProjector> textureProjectors;
+  private GeometryList targetGeometryList;  
+
+  public TextureProjectorRenderer1pass(AssetManager assetManager) 
+  { 
+    textureMat = new Material(assetManager, "Common/MatDefs/Misc/ProjectiveTextureMapping1pass.j3md");
+    textureProjectors = new ArrayList<TextureProjector>();
+    renderManager = null;
+    viewPort = null;
+    targetGeometryList = null;
+  }
+  
+  /**
+   * @return A list of TextureProjectors rendered by this instance.
+   */  
+  public List<TextureProjector> getTextureProjectors()
+  {
+    return textureProjectors;
+  }
+
+  /**
+   * @return The geometry that should be affected by this TextureProjector.
+   * Return null if the whole RenderQueue shall be affected.
+   * @see TextureProjector
+   */  
+  public GeometryList getTargetGeometryList() 
+  {
+    return this.targetGeometryList;
+  }
+  
+  /**
+   * Sets the geometry that sould be affected by this TextureProjector.
+   * @param The geometry that sould be affected by this TextureProjector.
+   * @see TextureProjector
+   */  
+  public void setTargetGeometryList(GeometryList targetGeometryList) 
+  {
+    this.targetGeometryList = targetGeometryList;
+  }
+  
+  /**
+   * Initializes this instance.
+   * @see SceneProcessor
+   */  
+  @Override
+  public void initialize(RenderManager rm, ViewPort vp) 
+  {
+    renderManager = rm;
+    viewPort = vp;
+  }
+
+  /**
+   * @return true, if this instance is initialized, false otherwise.
+   * @see SceneProcessor
+   */  
+  @Override
+  public boolean isInitialized() 
+  {
+    return viewPort != null;
+  }
+
+  /**
+   * Called before the a frame is rendered.
+   * @see SceneProcessor
+   */  
+  @Override
+  public void preFrame(float tpf) { }
+  
+  /**
+   * Called before the render queue is flushed.
+   * @see SceneProcessor
+   */  
+  @Override
+  public void postQueue(RenderQueue rq) 
+  {
+  }
+
+  /**
+   * Renders each TextureProjector with its corresponding material parameters.
+   * Called after a frame has been rendered. 
+   * @see SceneProcessor
+   */  
+  @Override
+  public void postFrame(FrameBuffer out) 
+  { 
+    int numProjectors = textureProjectors.size();    
+    textureMat.setInt("NumProjectors", numProjectors);
+    
+    for (int i = 0; i < numProjectors; i++)
+    {
+      TextureProjector textureProjector = textureProjectors.get(i);
+      
+      textureMat.setTexture("ProjectiveMap" + i, textureProjector.getProjectiveTexture());
+      textureMat.setMatrix4("ProjectorViewProjectionMatrix" + i, textureProjector.getProjectorViewProjectionMatrix());
+      
+      if (textureProjector.isParallelProjection())
+      {
+        textureMat.clearParam("ProjectorLocation" + i);
+        textureMat.setVector3("ProjectorDirection" + i, textureProjector.getProjectorDirection());
+      }
+      else
+      {
+        textureMat.clearParam("ProjectorDirection" + i);
+        textureMat.setVector3("ProjectorLocation" + i, textureProjector.getProjectorLocation());
+      }           
+    }
+
+    renderManager.setForcedMaterial(textureMat);
+    
+    if (targetGeometryList != null)
+    {
+      renderManager.renderGeometryList(targetGeometryList);
+    }
+    else
+    {
+      renderManager.renderViewPortRaw(viewPort);
+    }
+    
+    renderManager.setForcedMaterial(null);
+  }
+
+  /**
+   * Cleans up this instance.
+   * @see SceneProcessor
+   */  
+  @Override
+  public void cleanup() 
+  {
+  }
+
+  /**
+   * Called if the shape of the ViewPort changed.
+   * @see SceneProcessor
+   */  
+  @Override
+  public void reshape(ViewPort vp, int w, int h) 
+  {
+  }  
+}
